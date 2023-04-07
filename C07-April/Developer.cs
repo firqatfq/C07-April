84f02c82483d17435f920d3a58b9f2268b5ee615
Firqat F. Qasimov <Firqatfq@code.local>
2023-04-07 17:51:44 +0400

    initial commit

---
+++ C07-April/Developer.cs
@@ -0,0 +1,34 @@
+﻿internal class Developer
+    {
+
+    public string Name;
+    public string Surname;
+    public int Age;
+    public double Salary;
+    public string Position;
+
+    public Developer():this("Guest")
+    {
+
+    }
+    public Developer(string name)
+    {
+        this.Name = name;
+    }
+    public Developer(string name, string surname) : this(name)
+    {
+        this.Surname = surname;
+    }
+    public Developer(string name, string surname, int age, double salary, string position) : this(name, surname)
+    {
+        Age = age;
+        Salary = salary;
+        Position = position;
+    }
+
+    public void GetAllInfo()
+    {
+        Console.WriteLine($"{Name} {Surname} {Age} {Salary} {Position}");
+    }
+    
+    }
---
+++ C07-April/Program.cs
@@ -0,0 +1,10 @@
+﻿Developer DevA = new Developer("Firgat", "Gasimov", 25, 150000, "Engineer");
+Developer DevB = new Developer();
+Developer DevC = new Developer();
+Developer DevD = new Developer();
+Developer DevE = new Developer();
+
+
+
+//Console.WriteLine($"{DevA.Name} {DevA.Surname}");
+DevA.GetAllInfo();
